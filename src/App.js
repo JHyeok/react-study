ca2793637537f480d301227780a631acde8154ff
JHyeok <kjh2522k@gmail.com>
2019-02-03 13:30:42 +0900

    Create Phone-Book

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
-import Counter from './Counter'
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김재혁',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '개발자',
+        phone: '010-0000-0000'
+      }
+    ]
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+        ? { ...info, ...data }  // 새 객체 만들어서 기존의 값과 전달받은 data 값 덮어씀
+        : info  // 기존 값 유지
+      )
+    })
+  }
   render() {
     return (
-      <Counter />
+      <div>
+        <PhoneForm
+        onCreate={this.handleCreate}
+        />
+        <PhoneInfoList
+        data={this.state.information}
+        onRemove={this.handleRemove}
+        onUpdate={this.handleUpdate}
+        />
+      </div>
     );
   }
 }
